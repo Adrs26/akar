372834f254f1eb3bbffc59050220d833c7501bed
Adrian Septiyadi <adrianseptiyadi@gmail.com>
2026-06-18 08:52:55 +0700

    feat(core): integrate custom font families, typography styles and theme wrapper

---
+++ core/src/commonMain/kotlin/dev/adrian/akar/core/theme/AkarTheme.kt
@@ -0,0 +1,96 @@
+package dev.adrian.akar.core.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import dev.adrian.akar.core.color.AkarColorScheme
+import dev.adrian.akar.core.color.LocalAkarColor
+import dev.adrian.akar.core.color.akarDarkColorScheme
+import dev.adrian.akar.core.color.akarLightColorScheme
+import dev.adrian.akar.core.shape.AkarShape
+import dev.adrian.akar.core.shape.LocalAkarShape
+import dev.adrian.akar.core.shape.akarShape
+import dev.adrian.akar.core.spacing.AkarSpacing
+import dev.adrian.akar.core.spacing.LocalAkarSpacing
+import dev.adrian.akar.core.spacing.akarSpacing
+import dev.adrian.akar.core.typography.AkarTypography
+import dev.adrian.akar.core.typography.LocalAkarTypography
+import dev.adrian.akar.core.typography.akarTypography
+
+@Composable
+fun AkarTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit,
+) {
+    val akarColorScheme = if (darkTheme) akarDarkColorScheme else akarLightColorScheme
+    val materialColorScheme = when {
+        darkTheme -> {
+            darkColorScheme(
+                primary = akarColorScheme.primary,
+                onPrimary = akarColorScheme.onPrimary,
+                background = akarColorScheme.background,
+                onBackground = akarColorScheme.onBackground,
+                surface = akarColorScheme.surface,
+                surfaceContainerHigh = akarColorScheme.surface,
+                onSurface = akarColorScheme.onSurface,
+                inverseOnSurface = akarColorScheme.surface,
+                error = akarColorScheme.error,
+            )
+        }
+        else -> {
+            lightColorScheme(
+                primary = akarColorScheme.primary,
+                onPrimary = akarColorScheme.onPrimary,
+                background = akarColorScheme.background,
+                onBackground = akarColorScheme.onBackground,
+                surface = akarColorScheme.surface,
+                surfaceContainerHigh = akarColorScheme.surface,
+                onSurface = akarColorScheme.onSurface,
+                inverseOnSurface = akarColorScheme.surface,
+                error = akarColorScheme.error,
+            )
+        }
+    }
+
+    val materialTypography = Typography(
+        bodyLarge = akarTypography().bodyFeatured,
+        bodyMedium = akarTypography().labelFeatured,
+        labelMedium = akarTypography().labelStandard,
+        labelSmall = akarTypography().labelSubtle
+    )
+
+    CompositionLocalProvider(
+        LocalAkarColor provides akarColorScheme,
+        LocalAkarShape provides akarShape,
+        LocalAkarTypography provides akarTypography(),
+        LocalAkarSpacing provides akarSpacing
+    ) {
+        MaterialTheme(
+            colorScheme = materialColorScheme,
+            motionScheme = MaterialTheme.motionScheme,
+            shapes = MaterialTheme.shapes,
+            typography = materialTypography,
+            content = content
+        )
+    }
+}
+
+object AkarTheme {
+
+    val colors: AkarColorScheme
+        @Composable @ReadOnlyComposable get() = LocalAkarColor.current
+
+    val shapes: AkarShape
+        @Composable @ReadOnlyComposable get() = LocalAkarShape.current
+
+    val typography: AkarTypography
+        @Composable @ReadOnlyComposable get() = LocalAkarTypography.current
+
+    val spacing: AkarSpacing
+        @Composable @ReadOnlyComposable get() = LocalAkarSpacing.current
+}
---
+++ core/src/commonMain/kotlin/dev/adrian/akar/core/typography/AkarTypography.kt
@@ -0,0 +1,168 @@
+package dev.adrian.akar.core.typography
+
+import akar.core.generated.resources.Res
+import akar.core.generated.resources.plus_jakarta_sans_bold
+import akar.core.generated.resources.plus_jakarta_sans_extra_bold
+import akar.core.generated.resources.plus_jakarta_sans_medium
+import akar.core.generated.resources.plus_jakarta_sans_regular
+import akar.core.generated.resources.plus_jakarta_sans_semi_bold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.resources.Font
+
+@Immutable
+data class AkarTypography(
+    val headingStandard: TextStyle,
+    val headingSubtle: TextStyle,
+    val titleFeatured: TextStyle,
+    val titleStandard: TextStyle,
+    val titleSubtle: TextStyle,
+    val numberFeatured: TextStyle,
+    val numberStandard: TextStyle,
+    val numberSubtle: TextStyle,
+    val bodyFeatured: TextStyle,
+    val bodyStandard: TextStyle,
+    val captionStandard: TextStyle,
+    val labelFeatured: TextStyle,
+    val labelStandard: TextStyle,
+    val labelSubtle: TextStyle,
+    val actionFeatured: TextStyle,
+    val actionStandard: TextStyle,
+    val actionSubtle: TextStyle
+)
+
+@Composable
+private fun plusJakartaSansFontFamily() = FontFamily(
+    Font(Res.font.plus_jakarta_sans_regular, FontWeight.Normal),
+    Font(Res.font.plus_jakarta_sans_medium, FontWeight.Medium),
+    Font(Res.font.plus_jakarta_sans_semi_bold, FontWeight.SemiBold),
+    Font(Res.font.plus_jakarta_sans_bold, FontWeight.Bold),
+    Font(Res.font.plus_jakarta_sans_extra_bold, FontWeight.ExtraBold)
+)
+
+@Composable
+fun akarTypography() = AkarTypography(
+    headingStandard = TextStyle(
+        fontFamily = plusJakartaSansFontFamily(),
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
+        letterSpacing = 0.5.sp
+    ),
+    headingSubtle = TextStyle(
+        fontFamily = plusJakartaSansFontFamily(),
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleFeatured = TextStyle(
+        fontFamily = plusJakartaSansFontFamily(),
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleStandard = TextStyle(
+        fontFamily = plusJakartaSansFontFamily(),
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleSubtle = TextStyle(
+        fontFamily = plusJakartaSansFontFamily(),
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        letterSpacing = 0.5.sp
+    ),
+    numberFeatured = TextStyle(
+        fontFamily = plusJakartaSansFontFamily(),
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        letterSpacing = 0.5.sp,
+        fontFeatureSettings = "tnum"
+    ),
+    numberStandard = TextStyle(
+        fontFamily = plusJakartaSansFontFamily(),
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        letterSpacing = 0.5.sp,
+        fontFeatureSettings = "tnum"
+    ),
+    numberSubtle = TextStyle(
+        fontFamily = plusJakartaSansFontFamily(),
+        fontWeight = FontWeight.Bold,
+        fontSize = 12.sp,
+        letterSpacing = 0.5.sp,
+        fontFeatureSettings = "tnum"
+    ),
+    bodyFeatured = TextStyle(
+        fontFamily = plusJakartaSansFontFamily(),
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        letterSpacing = 0.5.sp,
+        lineHeight = 21.sp
+    ),
+    bodyStandard = TextStyle(
+        fontFamily = plusJakartaSansFontFamily(),
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        letterSpacing = 0.5.sp,
+        lineHeight = 21.sp
+    ),
+    captionStandard = TextStyle(
+        fontFamily = plusJakartaSansFontFamily(),
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        letterSpacing = 0.5.sp,
+        lineHeight = 18.sp
+    ),
+    labelFeatured = TextStyle(
+        fontFamily = plusJakartaSansFontFamily(),
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 12.sp,
+        letterSpacing = 0.5.sp,
+        lineHeight = 18.sp
+    ),
+    labelStandard = TextStyle(
+        fontFamily = plusJakartaSansFontFamily(),
+        fontWeight = FontWeight.Medium,
+        fontSize = 12.sp,
+        letterSpacing = 0.5.sp,
+        lineHeight = 18.sp
+    ),
+    labelSubtle = TextStyle(
+        fontFamily = plusJakartaSansFontFamily(),
+        fontWeight = FontWeight.Bold,
+        fontSize = 9.sp,
+        letterSpacing = 0.5.sp,
+        lineHeight = 16.sp
+    ),
+    actionFeatured = TextStyle(
+        fontFamily = plusJakartaSansFontFamily(),
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        letterSpacing = 0.5.sp,
+        lineHeight = 21.sp
+    ),
+    actionStandard = TextStyle(
+        fontFamily = plusJakartaSansFontFamily(),
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 12.sp,
+        letterSpacing = 0.5.sp,
+        lineHeight = 18.sp
+    ),
+    actionSubtle = TextStyle(
+        fontFamily = plusJakartaSansFontFamily(),
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        letterSpacing = 0.5.sp,
+        lineHeight = 16.sp
+    ),
+)
+
+val LocalAkarTypography = staticCompositionLocalOf<AkarTypography> {
+    error("No typography provided")
+}
