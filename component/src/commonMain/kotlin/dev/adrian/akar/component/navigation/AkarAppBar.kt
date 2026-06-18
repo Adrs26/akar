5d7815c694eff08e316be215c64125e08cd798da
Adrian Septiyadi <adrianseptiyadi@gmail.com>
2026-06-18 11:49:36 +0700

    feat(component) add AkarAppBar component

---
+++ component/src/commonMain/kotlin/dev/adrian/akar/component/navigation/AkarAppBar.kt
@@ -0,0 +1,35 @@
+package dev.adrian.akar.component.navigation
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import dev.adrian.akar.core.theme.AkarTheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AkarAppBar(
+    title: String,
+    modifier: Modifier = Modifier,
+    navigationIcon: @Composable (() -> Unit)? = null,
+    actions: @Composable RowScope.() -> Unit = {},
+    textColor: Color = AkarTheme.colors.onSurface,
+) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+                color = textColor,
+                style = AkarTheme.typography.headingStandard
+            )
+        },
+        modifier = modifier,
+        navigationIcon = { navigationIcon?.invoke() },
+        actions = actions,
+        colors = TopAppBarDefaults.topAppBarColors(AkarTheme.colors.surface)
+    )
+}
