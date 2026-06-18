d70c21330dc154a9182f1bdbfce63f3572496079
Adrian Septiyadi <adrianseptiyadi@gmail.com>
2026-06-18 14:24:23 +0700

    feat(component) add AkarFloatingActionButton component

---
+++ component/src/commonMain/kotlin/dev/adrian/akar/component/button/AkarFloatingActionButton.kt
@@ -0,0 +1,64 @@
+package dev.adrian.akar.component.button
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import dev.adrian.akar.component.feedback.AkarIcon
+import dev.adrian.akar.core.theme.AkarTheme
+import kotlin.time.Clock
+
+@Composable
+fun AkarFloatingActionButton(
+    icon: Char,
+    contentDescription: String?,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
+
+    var lastClickTime by remember { mutableLongStateOf(0L) }
+
+    val scale by animateFloatAsState(
+        targetValue = if (isPressed) 0.95f else 1.0f,
+        animationSpec = spring(
+            dampingRatio = 0.6f,
+            stiffness = 400f
+        ),
+        label = "AkarButtonScaleAnimation"
+    )
+
+    FloatingActionButton(
+        onClick = {
+            val currentTime = Clock.System.now().toEpochMilliseconds()
+            if (currentTime - lastClickTime >= 700L) {
+                lastClickTime = currentTime
+                onClick()
+            }
+        },
+        modifier = modifier.graphicsLayer {
+            scaleX = scale
+            scaleY = scale
+        },
+        shape = AkarTheme.shapes.full,
+        containerColor = AkarTheme.colors.primary,
+        contentColor = AkarTheme.colors.onPrimary,
+        interactionSource = interactionSource
+    ) {
+        AkarIcon(
+            icon = icon,
+            contentDescription = contentDescription,
+            modifier = modifier,
+            tint = AkarTheme.colors.onPrimary,
+        )
+    }
+}
--- shared/src/commonMain/kotlin/dev/adrian/akar/App.kt
+++ shared/src/commonMain/kotlin/dev/adrian/akar/App.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -11,10 +12,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.adrian.akar.component.button.AkarButton
+import dev.adrian.akar.component.button.AkarFloatingActionButton
 import dev.adrian.akar.component.button.AkarIconButton
 import dev.adrian.akar.component.button.AkarOutlinedButton
 import dev.adrian.akar.component.feedback.AkarIcon
 import dev.adrian.akar.component.feedback.AkarIconBox
+import dev.adrian.akar.component.layout.AkarScaffold
 import dev.adrian.akar.core.theme.AkarTheme
 import dev.adrian.akar.icon.AkarIcons
 
@@ -27,39 +30,51 @@ fun App() {
     AkarTheme(
         icon = icon
     ) {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(AkarTheme.spacing.medium),
-                horizontalAlignment = Alignment.CenterHorizontally
+        AkarScaffold(
+            floatingActionButton = {
+                AkarFloatingActionButton(
+                    icon = AkarIcons.Add2,
+                    contentDescription = null,
+                    onClick = {  }
+                )
+            }
+        ) { innerPadding ->
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                contentAlignment = Alignment.Center
             ) {
-                AkarIconBox(
-                    icon = AkarIcons.EmojiFoodBeverage,
-                    contentDescription = null,
-                    supportingIcon = {
-                        AkarIcon(
-                            icon = AkarIcons.Lock,
-                            contentDescription = null,
-                            modifier = Modifier.size(16.dp),
-                            iconFont = filledIcon
-                        )
-                    }
-                )
-                AkarButton(
-                    label = "Press me",
-                    onClick = {  }
-                )
-                AkarOutlinedButton(
-                    label = "Press me",
-                    onClick = {  }
-                )
-                AkarIconButton(
-                    icon = AkarIcons.Settings,
-                    contentDescription = null,
-                    onClick = {  }
-                )
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(AkarTheme.spacing.medium),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    AkarIconBox(
+                        icon = AkarIcons.EmojiFoodBeverage,
+                        contentDescription = null,
+                        supportingIcon = {
+                            AkarIcon(
+                                icon = AkarIcons.Lock,
+                                contentDescription = null,
+                                modifier = Modifier.size(16.dp),
+                                iconFont = filledIcon
+                            )
+                        }
+                    )
+                    AkarButton(
+                        label = "Press me",
+                        onClick = {  }
+                    )
+                    AkarOutlinedButton(
+                        label = "Press me",
+                        onClick = {  }
+                    )
+                    AkarIconButton(
+                        icon = AkarIcons.Settings,
+                        contentDescription = null,
+                        onClick = {  }
+                    )
+                }
             }
         }
     }
